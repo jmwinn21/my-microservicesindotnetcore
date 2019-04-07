06a978e0df4b8b5244915af997f5698214c3fb5d
Jacob Winn <jmwinn21@gmail.com>
2019-04-07 15:15:33 -0400

    finish chapter 1 - HelloMicroservices
    '

---
+++ chapter1/HelloMicroservices/CurrentDateTimeModule.cs
@@ -0,0 +1,10 @@
+using System;
+using Nancy;
+
+namespace HelloMicroservices {
+    public class CurrentDateTimeModule : NancyModule {
+        public CurrentDateTimeModule () {
+            Get ("/", _ => DateTime.UtcNow);
+        }
+    }
+}
